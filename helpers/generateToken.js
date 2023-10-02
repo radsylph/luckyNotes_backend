56152694d102fb5721ec1cba8fa0c9811ad9231e
andresa <andresa@devtop.io>
2023-10-02 16:19:16 -0400

    feat:created helpers for token generation and mail sending (stil wip)

---
+++ helpers/generateToken.js
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
+
+const generateToken1 = () => {
+  const token = crypto.randomBytes(10).toString("hex");
+  console.log(token);
+  return token;
+};
+
+const generateJWT = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+/*const generateToken2 = () => {
+  let x = Math.random().toString(36).substring(2, 15) + Date.now().toString(32);
+  console.log(x);
+  return x;
+};*/
+
+export { generateToken1, generateJWT };
---
+++ helpers/mails.js
@@ -0,0 +1,64 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const emailRegistro = async (datos) => {
+  var transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_HOST_USER,
+      pass: process.env.EMAIL_HOST_PASSWORD,
+    },
+  });
+
+  console.log(datos);
+
+  const { email, nombre, token } = datos;
+  try {
+    await transport.sendMail({
+      from: "lo que sea",
+      to: email,
+      subject: "Confirm your account",
+      html: `Hi ${nombre}, your account is almost ready to be created</p>
+      <p>you just need to verify your account using the next link:
+      <a href="${process.env.BACKEND_URL}:${
+        process.env.PORT ?? 3000
+      }/auth/confirmar/${token}">Verify your account</a></p>
+      <p>if you didnt create this account, pls dont pay attention to this message</p>`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const emailReset = async (datos) => {
+  var transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_HOST_USER,
+      pass: process.env.EMAIL_HOST_PASSWORD,
+    },
+  });
+
+  const { email, nombre, token } = datos;
+  try {
+    await transport.sendMail({
+      from: "lo que sea",
+      to: email,
+      subject: "Reset your password",
+      html: `Hi ${nombre}, this is your email confirmation to rest your password</p>
+      <p>you just need to verify your account using the next link:
+      <a href="${process.env.BACKEND_URL}:${
+        process.env.PORT ?? 3000
+      }/auth/reset/${token}">Reset your password</a></p>
+      <p>if you didnt create this account, pls dont pay attention to this message</p>`,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+//http://localhost:3000/confirm/${token}
+export { emailRegistro, emailReset };
