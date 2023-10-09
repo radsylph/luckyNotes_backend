151b99dd43243c9e9e01f3ccebb5f8059e3f5140
radsylph <andresa@devtop.io>
2023-10-08 22:04:43 -0400

    feat:fixed cors problem

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(express.static("public"));
 app.use("/auth", router);
 
