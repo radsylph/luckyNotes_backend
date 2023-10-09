904c8832c381710e320a8f9d39f492678bae2f0d
radsylph <andresa@devtop.io>
2023-10-09 07:48:02 -0400

    feat: added new routes for user verify

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,15 @@
 import Express from "express";
-import { createUser, createForm } from "../controllers/userControllers.js";
+import {
+  createUser,
+  createForm,
+  confirmAccount,
+  testingpug,
+} from "../controllers/userControllers.js";
 
 const router = Express.Router();
 
 router.route("/create").get(createForm).post(createUser);
+router.route("/confirm/:token").get(confirmAccount);
+router.route("/testingpug").get(testingpug);
+
 export default router;
