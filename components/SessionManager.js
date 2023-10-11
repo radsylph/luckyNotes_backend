ebf39c5cc7065fdebf09434ef393d1c1d2712fc7
radsylph <andresa@devtop.io>
2023-10-10 21:41:53 -0400

    feat: added validations to the back with express-validator

--- components/SessionManager.js
+++ components/SessionManager.js
@@ -2,11 +2,42 @@ import Usuario from "../models/Usuario.js";
 import bcrypt from "bcrypt";
 import { generateToken1, generateJWT } from "../helpers/generateToken.js";
 import { emailRegistro } from "../helpers/mails.js";
+import { check, validationResult } from "express-validator";
 
 class SessionManager {
   constructor() {}
 
   async createUser(req, res) {
+    await check("name").notEmpty().withMessage("Name is required").run(req);
+    await check("lastname")
+      .notEmpty()
+      .withMessage("Lastname is required")
+      .run(req);
+    await check("username")
+      .notEmpty()
+      .withMessage("Username is required")
+      .run(req);
+    await check("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is not valid")
+      .run(req);
+    await check("password")
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long")
+      .run(req);
+
+    let result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({
+        message: "Error al crear el usuario",
+        error: result.array(),
+      });
+    }
+
     console.log(req.body);
     try {
       const { name, lastname, username, email, password } = req.body;
