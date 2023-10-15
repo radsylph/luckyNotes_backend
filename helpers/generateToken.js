ca48b8cfae0614bfb8b9e36ed18a4177c1ccde31
radsylph <andresa@devtop.io>
2023-10-15 17:30:21 -0400

    feat: wip token methods

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -1,5 +1,6 @@
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const generateToken1 = () => {
   const token = crypto.randomBytes(10).toString("hex");
@@ -12,10 +13,27 @@ const generateJWT = (id) => {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
+
+const storeToken = async (token) => {
+  try {
+    await AsyncStorage.setItem("token", token);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeToken = async () => {
+  try {
+    await AsyncStorage.removeItem("token");
+  } catch (e) {
+    // Manejo de errores (puedes mostrar un mensaje de error al usuario)
+  }
+};
+
 /*const generateToken2 = () => {
   let x = Math.random().toString(36).substring(2, 15) + Date.now().toString(32);
   console.log(x);
   return x;
 };*/
 
-export { generateToken1, generateJWT };
+export { generateToken1, generateJWT, storeToken, removeToken };
