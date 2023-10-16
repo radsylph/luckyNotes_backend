90d244465184bca897f9ac81756bc738f13dabfc
radsylph <andresa@devtop.io>
2023-10-16 00:28:22 -0400

    test

--- components/SessionManager.js
+++ components/SessionManager.js
@@ -57,30 +57,26 @@ class SessionManager {
       if (existeUsername) {
         return res.status(400).json({
           message: "there was these errors",
-          errors: [
-            {
-              type: "field",
-              value: username,
-              msg: "the username is already registered",
-              path: "username",
-              location: "body",
-            },
-          ],
+          errors: {
+            type: "field",
+            value: username,
+            msg: "the username is already registered",
+            path: "username",
+            location: "body",
+          },
         });
       }
 
       if (ExisteUsuario) {
         return res.status(400).json({
           message: "there was these errors",
-          errors: [
-            {
-              type: "field",
-              value: email,
-              msg: "the email is already registered",
-              path: "email",
-              location: "body",
-            },
-          ],
+          errors: {
+            type: "field",
+            value: email,
+            msg: "the email is already registered",
+            path: "email",
+            location: "body",
+          },
         });
       }
 
