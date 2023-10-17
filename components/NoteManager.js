203daece0a209576e022cde78f36daa32a81ccc4
radsylph <andresa@devtop.io>
2023-10-16 22:08:02 -0400

    create noteManager

---
+++ components/NoteManager.js
@@ -0,0 +1,43 @@
+import Note from "../models/Nota.js";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+class NoteManager {
+  constructor() {}
+
+  async createNote(req, res) {
+    await check("title")
+      .notEmpty()
+      .withMessage("Title is required")
+      .isLength({ max: 300 })
+      .withMessage("The title is to long")
+      .run(req);
+    await check("content")
+      .notEmpty()
+      .withMessage("Content is required")
+      .run(req);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "you have the following errors",
+        errors: errors.array(),
+      });
+    }
+    const { title, content, SerieId, favorite, trash, owner } = req.body;
+    const decoded = jwt.verify(owner, process.env.SECRET);
+    console.log(decoded);
+    return res.status(201).json({
+      message: "Note created",
+      status: 201,
+    });
+  }
+
+  async test(req, res) {
+    return res.status(201).json({
+      message: "Note created",
+      status: 201,
+    });
+  }
+}
+
+export { NoteManager };
