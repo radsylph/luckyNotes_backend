49a60d41c5a0c542d829bc28c79686ec67b99c1e
radsylph <andresa@devtop.io>
2023-10-17 22:56:40 -0400

    feat: added create series,add note to series, and show series methods and some extra validations

--- components/NoteManager.js
+++ components/NoteManager.js
@@ -1,5 +1,6 @@
 import Note from "../models/Nota.js";
 import Usuario from "../models/Usuario.js";
+import Serie from "../models/Serie.js";
 import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
 import dotenv from "dotenv";
@@ -83,8 +84,22 @@ class NoteManager {
     }
     const { title, content, SerieId, favorite, trash } = req.body;
     const { owner } = req.user._id;
+    const serie = await Serie.findOne({ Name: SerieId });
+    if (!serie) {
+      return res.status(404).json({
+        message: "Serie not found",
+        status: 404,
+      });
+    }
+    if (req.user._id != serie.owner) {
+      return res.status(403).json({
+        message: "You are not authorized to edit this serie",
+        status: 403,
+      });
+    }
 
     try {
+      //que busque primero el nombre de la carpeta y si no existe que la cree
       const note = new Note({
         title,
         content,
@@ -176,6 +191,125 @@ class NoteManager {
   async showNotes(req, res) {
     try {
       const notes = await Note.find({ owner: req.user._id });
+      if (!notes) {
+        return res.status(404).json({
+          message: "Notes not found",
+          status: 404,
+        });
+      }
+      return res.status(200).json({
+        message: "Notes found",
+        status: 200,
+        notes,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error showing notes",
+        status: 500,
+        error,
+      });
+    }
+  }
+
+  async createSerie(req, res) {
+    await check("Name").notEmpty().withMessage("Name is required").run(req);
+    await check("Description")
+      .notEmpty()
+      .withMessage("Description is required")
+      .run(req);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        message: "you have the following errors",
+        errors: errors.array(),
+      });
+    }
+    const { Name, Description } = req.body;
+    try {
+      const serie = new Serie({
+        Name,
+        Description,
+        owner: req.user._id,
+      });
+      await serie.save();
+
+      return res.status(200).json({
+        message: "Serie created",
+        status: 200,
+        serie_info: {
+          Name,
+          Description,
+          owner: req.user._id,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error creating serie",
+        status: 500,
+        error,
+      });
+    }
+  }
+
+  async addNoteToSerie(req, res) {
+    const { id } = req.params;
+    const { SerieId } = req.body;
+    try {
+      const serie = await Serie.findOne({ Name: SerieId });
+      if (!serie) {
+        return res.status(404).json({
+          message: "Serie not found",
+          status: 404,
+        });
+      }
+      if (req.user._id != serie.owner) {
+        return res.status(403).json({
+          message: "You are not authorized to edit this serie",
+          status: 403,
+        });
+      }
+
+      const note = await Note.findOneAndUpdate(
+        { _id: id, owner: req.user._id },
+        { SerieId },
+        { new: true }
+      );
+      if (!note) {
+        return res.status(404).json({
+          message: "Note not found",
+          status: 404,
+        });
+      }
+      if (req.user._id != note.owner) {
+        return res.status(403).json({
+          message: "You are not authorized to edit this note",
+          status: 403,
+        });
+      }
+      await note.save();
+      await serie.save();
+      return res.status(200).json({
+        message: "Note Added to the series",
+        status: 200,
+        note,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        message: "Error updating note",
+        status: 500,
+        error,
+      });
+    }
+  }
+
+  async showSerieNotes(req, res) {
+    const { SerieId } = req.body;
+    try {
+      const notes = await Note.find({ owner: req.user._id, SerieId });
       if (!notes) {
         return res.status(404).json({
           message: "Notes not found",
