74eb56fe7de44301b6414dc94d7fe0bfb7878deb
radsylph <andresa@devtop.io>
2023-10-19 00:58:10 -0400

    feat: delete empty route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -20,5 +20,5 @@ router.route("/confirm/:token").get(confirmAccount);
 router.route("/testingpug").get(testingpug);
 router.route("/reset_password").get(formReset).post(resetPassword);
 router.route("/reset_password/:token").get(verifyPassword).post(newPassword);
-router.route("reset_passwordpug/:token");
+
 export default router;
