9a985382ef0bb69b0c6d53c558c81767e5ff5ee7
radsylph <andresa@devtop.io>
2023-10-18 22:52:35 -0400

    feat: update mail html template

--- helpers/mails.js
+++ helpers/mails.js
@@ -15,9 +15,10 @@ const emailRegistro = async (datos) => {
 
   const transport = nodemailer.createTransport({
     service: "gmail",
-    auth: {},
-    user: process.env.APP_USER,
-    pass: process.env.APP_PASSWORD,
+    auth: {
+      user: process.env.APP_USER,
+      pass: process.env.APP_PASSWORD,
+    },
   });
 
   console.log(datos);
@@ -28,10 +29,57 @@ const emailRegistro = async (datos) => {
       from: process.env.APP_USER,
       to: email,
       subject: "Confirm your account",
-      html: `Hi ${nombre}, your account is almost ready to be created</p>
-      <p>you just need to verify your account using the next link:
-      <a href="${process.env.BACKEND_URL}/auth/confirm/${token}">Verify your account</a></p>
-      <p>if you didnt create this account, pls dont pay attention to this message</p>`,
+      html: `<!DOCTYPE html>
+      <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com馃彚office">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="x-apple-disable-message-reformatting">
+        <title></title>
+        <!--[if mso]>
+        <noscript>
+          <xml>
+            <o:OfficeDocumentSettings>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+          </xml>
+        </noscript>
+        <![endif]-->
+        <style>
+          table, td, div, h1, p {font-family: Arial, sans-serif;}
+        </style>
+      </head>
+      <body style="margin:0;padding:0;">
+        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#E4EBEC;">
+          <tr>
+            <td align="center" style="padding:0;">
+              <table role="presentation" style="with:602px;border-collapse:collapse;border:1px solid #0CA58A;border-spacing:0;text-align:left;">
+                <tr>
+                  <td align="center" style="padding:40px 0 30px 0;background: linear-gradient(25deg, #DFC592 0%, #0CA58A 100%);">
+                    <a href="#"> <img src="https://i.postimg.cc/9QVpjXs8/2.png" alt="" width="200" style="height:auto;display:block;" /></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="padding:36px 30px 20px 30px; ">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="padding:0 0 0 0;color:#112E29;">
+                          <h1 align="center" style="font-size:24px;margin:0 0 20px 0;font-family: Arial, sans-serif;color:#0CA58A;  font-size: 30px;">Verify your account </h1>
+                          <h1 style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: arial,helvetica,sans-serif; font-size: 16px; color:#0CA58A;">
+                            <div><strong>Use the following link to verify your account:<a href="${process.env.BACKEND_URL}/auth/confirm/${token}" style="font-size: 30px; margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, sans-serif;color:#112E29;"> Verify your account</a>  </strong></div>
+                          </h1>
+                          <br>
+                          <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, sans-serif;color:#112E29;">if you didnt create this account, pls dont pay attention to this message</p>
+                        </td>
+                      </tr>
+                    
+                    </table>
+                  </td>
+                </tr>
+               
+          </table>
+        </body>
+        </html>`,
     });
   } catch (error) {
     console.log(error);
@@ -54,10 +102,59 @@ const emailReset = async (datos) => {
       from: "lo que sea",
       to: email,
       subject: "Reset your password",
-      html: `Hi ${nombre}, this is your email confirmation to rest your password</p>
-      <p>you just need to verify your account using the next link:
-      <a href="${process.env.BACKEND_URL}/auth/reset_password/${token}">Reset your password</a></p>
-      <p>if you didnt create this account, pls dont pay attention to this message</p>`,
+      html: `<!DOCTYPE html>
+      <html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta name="x-apple-disable-message-reformatting">
+        <title></title>
+        <!--[if mso]>
+        <noscript>
+          <xml>
+            <o:OfficeDocumentSettings>
+              <o:PixelsPerInch>96</o:PixelsPerInch>
+            </o:OfficeDocumentSettings>
+          </xml>
+        </noscript>
+        <![endif]-->
+        <style>
+          table, td, div, h1, p {font-family: Arial, sans-serif;}
+        </style>
+      </head>
+      <body style="margin:0;padding:0;">
+        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#E4EBEC;">
+          <tr>
+            <td align="center" style="padding:0;">
+              <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #0CA58A;border-spacing:0;text-align:left;">
+                <tr>
+                  <td align="center" style="padding:40px 0 30px 0;background: linear-gradient(25deg, #DFC592 0%, #0CA58A 100%);">
+                    <a href="#"> <img src="https://i.postimg.cc/9QVpjXs8/2.png" alt="" width="200" style="height:auto;display:block;" /></a>
+                  </td>
+                </tr>
+                <tr>
+                  <td style="padding:36px 30px 20px 30px;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="padding:0 0 0 0;color:#112E29;">
+                          <h1 align="center" style="font-size:24px;margin:0 0 20px 0;font-family: Arial, sans-serif;color:#0CA58A;">Reset your password</h1>
+                          <h1 style="margin: 0px; line-height: 140%; text-align: center; word-wrap: break-word; font-weight: normal; font-family: arial,helvetica,sans-serif; font-size: 16px; color:#0CA58A;">
+                            <div><strong>Use the following link to reset your password: <a href="${process.env.BACKEND_URL}/auth/reset_password/${token}" style="color:#112E29;">Reset your password</a></strong></div>
+                          </h1>
+                          <br>
+                          <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family: Arial, sans-serif;color:#112E29;">If you didn't request a password reset, please ignore this message.</p>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>
+       `,
     });
   } catch (error) {
     console.log(error);
